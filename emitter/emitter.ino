ca376aae905a60a19c7ba3b8a4e2888781707e8a
Matias Carou <matiascarou1@gmail.com>
2024-08-16 00:18:08 -0300

    Add receiver + software reset

--- emitter/emitter.ino
+++ emitter/emitter.ino
@@ -2,18 +2,20 @@
 #include <Arduino.h>
 
 #include "Adafruit_VL53L0X.h"
+#include "Config.h"
 #include "I2Cdev.h"
 #include "MPU6050.h"
 #include "MidiSensor.h"
-#include "Config.h"
 
 std::vector<MidiSensor *> SENSORS = {};
 
+const int RESET_PIN = 2;
+
 void setup() {
-  delay(250);
   Serial.begin(9600);
 
   SENSORS = MidiSensor::initializeSensors(CONFIG);
+  pinMode(RESET_PIN, INPUT_PULLUP);
 
   analogReadResolution(10);
 
@@ -21,6 +23,10 @@ void setup() {
 }
 
 void loop() {
+  if (!digitalRead(RESET_PIN)) {
+    SCB_AIRCR = 0x05FA0004;
+  }
+
   for (MidiSensor *SENSOR : SENSORS) {
     if (!SENSOR->isSwitchActive()) {
       continue;
